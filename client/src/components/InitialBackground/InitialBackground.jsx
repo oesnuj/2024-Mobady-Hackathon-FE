bf57dbd53f8deeec5b9af6459f7c9c3412006c68
JangHongJoon <wkdghdwns19969@gmail.com>
2024-11-01 23:20:36 +0900

    feat : 하늘색 배경 컴포넌트 생성

---
+++ client/src/components/InitialBackground/InitialBackground.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const InitialBackground = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh; 
+    width: 100vw; 
+    background-color: #E6F8FF;
+`
+
+export {InitialBackground};
