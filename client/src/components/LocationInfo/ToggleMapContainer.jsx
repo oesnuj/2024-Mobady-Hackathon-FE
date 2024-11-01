b306b2aa3c3e4367cfd564ace49c7409b0324f1f
JangHongJoon <wkdghdwns19969@gmail.com>
2024-11-02 08:12:51 +0900

    feat : error 예외처리 및 특정 관광명소 자세한 정보 출력

---
+++ client/src/components/LocationInfo/ToggleMapContainer.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const ToggleMapContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
+  width: 90%;
+  height: 50%;
+  margin-top: 30px;
+`;
+export {ToggleMapContainer};
